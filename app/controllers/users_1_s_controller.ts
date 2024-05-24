3f9c28b15a1a30a9c1e6084c6346117f2018c2f6
Luuix02 <Lufvi02@gmail.com>
2024-05-24 13:33:56 -0500

    crud a medias controllers users_1_s

--- app/controllers/users_1_s_controller.ts
+++ app/controllers/users_1_s_controller.ts
@@ -1,50 +1,54 @@
+import UserService from '#services/user_service'
+import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
 
-export default class Users1SController {
-  /**
-   * Display a list of resource
-   * mostrar una lista de recursos
-   en este caso me mostrara la lista de los usuarios
-   */
-  async index({}: HttpContext) {}
 
-  /**
-   * Display form to create a new record
-   * Crear un nuevo registro
-   * se puede llamar a la db la info del usuario, cualquier atributo puede ser llamado en este paso
-   * cosas externas para crear un nuevo registro o data en la db
-   * 
-   */
+export default class Users1SController {
+
+   @inject()
+  async index({ response }: HttpContext,user_service: UserService) {
+    const users = await user_service.all()
+    return response.json(users)
+  }
+
   async create({}: HttpContext) {}
 
-  /**
-   * Handle form submission for the create action
-   * cuando el formulario se carga, lo hace a través de un btn para poder guardar esa info en la db
-   */
-  async store({ request }: HttpContext) {}
 
-  /**
-   * Show individual record
-   * es como buscar por el id, trae la data de un unico usuario
-   * 
-   */
-  async show({ params }: HttpContext) {}
+  async store({ request, response }: HttpContext, user_service: UserService) {
+    const data = request.only(['name','hincha'])
+    const user = await user_service.create(data)
+    return response.status(200).json(user)
+  }
 
-  /**
-   * Edit individual record
-   * trae los recursos para hacer la accion de editar
-   */
+  async show({ params, response }: HttpContext) {
+    const user = await User.find(params.id)
+    if(!user){
+      return response.status(404).json({message: 'User not found'})
+    }
+    return response.json(user)
+  }
+
   async edit({ params }: HttpContext) {}
 
-  /**
-   * Handle form submission for the edit action
-   * 
-   */
-  async update({ params, request }: HttpContext) {}
+ 
+  async update({ params, request, response }: HttpContext) {
+    const data = request.only(['fullName', 'email','password'])
+    const user = await User.find(params.id)
+    if(!user){
+      return response.status(404).json({message: 'User not found'})
+    }
+    user.merge(data)
+    await user.save()
+    return response.json(user)
+  }
 
-  /**
-   * Delete record
-   * eliminar
-   */
-  async destroy({ params }: HttpContext) {}
+
+  async destroy({ params, response }: HttpContext) {
+    const user = await User.find(params.id)
+    if(!user){
+      return response.status(404).json({message: 'User not found'})
+    }
+    await user.delete()
+    return response.status(204).json(null)
+  }
 }
